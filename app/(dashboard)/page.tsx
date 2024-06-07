b1b11bb1c4d33feb836bb24a5c248afa3ad91c9d
mattbreda <mattbreda@gmail.com>
2024-06-07 14:32:21 +0200

    First commit

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
+
+export default function Home() {
+  const {isOpen, onOpen} = useNewAccount()
+
+  return (
+    <div>
+      <Button onClick={onOpen}>Add an account</Button>
+    </div>
+  );
+}
